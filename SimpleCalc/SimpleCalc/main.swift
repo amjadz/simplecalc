b1cafe46aa3979a5711f9e41be04530bf6242d60
amjadz <amjadz@uw.edu>
2018-10-03 21:10:01 -0700

    Got the mod part

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -11,59 +11,73 @@ import Foundation
 public class Calculator {
     
     public func calculate(_ args: [String]) -> Int {
-        var allNums = args[args.count - 1]
+            var allNums = args[args.count - 1]
         
-}
+    }
 
-func remainderCalc(_ nums: [String]) -> Int {
-        <#function body#>
-}
-    
-func factorialCalc(_ nums: [String]) -> Int  {
-    if nums.count == 2 {
-        var theEnd = Int(nums[0])
-        var answer = 1;
-        for index in 1...theEnd!{
-            answer *= index
-        }
-        return answer
-    } else {
-        return 0
-    }
-}
-    
-func averageCalc(_ nums: [String]) ->  Int{
-    var average = 0;
-    var count = nums.count - 1
+    func remainderCalc(_ numberOne: Int, numberTwo: Int) -> Int {
+        var divsionRemainder = numberOne
+        var dividingNumber = numberTwo
         
-    if count > 0 {
-        var negative = false
-        for index in 0...nums.count - 2 {
-            if nums[index] == "-"{
-                negative = true
-                    
-            } else {
-                var nextNum = Int(nums[index])
-                if negative {
-                    nextNum = nextNum! * 1
-                    negative = false
-                        
-                }
-                    
-                average += nextNum!
-            }
-                
-            return average / count
-                
+        while divisonRemainder >= dividingNumber {
+            divsionRemainder -= dividingNumber
         }
-    } else {
-        return 0
+        return divsionRemainder
     }
-}
+
+    func factorialCalc(_ nums: [String]) -> Int  {
+        if nums.count == 2 {
+            var theEnd = Int(nums[0])
+            var answer = 1;
+            for index in 1...theEnd!{
+                answer *= index
+            }
+            return answer
+        } else {
+            return 0
+        }
+    }
+    
+    func averageCalc(_ nums: [String]) ->  Int{
+        var average = 0;
+        var count = nums.count - 1
+        
+        if count > 0 {
+            var negative = false
+            for index in 0...nums.count - 2 {
+                if nums[index] == "-"{
+                    negative = true
+                    
+                } else {
+                    var nextNum = Int(nums[index])
+                    if negative {
+                        nextNum = nextNum! * 1
+                        negative = false
+                        
+                    }
+                    
+                    average += nextNum!
+                }
+                
+                return average / count
+                
+            }
+        } else {
+            return 0
+        }
+    }
     
     
     
     public func calculate(_ arg: String) -> Int {
         return calculate( arg.split(separator: " ").map({ substr in String(substr) }) )
     }
+    
+print("UW Calculator v1")
+print("Enter an expression separated by returns:")
+let first = readLine()!
+let operation = readLine()!
+let second = readLine()!
+print(Calculator().calculate([first, operation, second]))
+
 }
