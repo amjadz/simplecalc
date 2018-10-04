fad6a3266b8f9ef00b554fde1b8caeff5b9e940e
Zubair A Amjad <amjadz@source-imac13.local>
2018-10-03 20:47:27 -0700

    Got the average part down

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,17 +10,50 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        var allNums = args[args.count - 1]
+        
     }
     
-    public func calculate(_ arg: String) -> Int {
-        return calculate( arg.split(separator: " ").map({ substr in String(substr) }) )
+func averageCalc(_ nums: [String]) ->  Int{
+    var average = 0;
+    var count = nums.count - 1
+        
+    if count > 0 {
+        var negative = false
+        for index in 0...nums.count - 2 {
+            if nums[index] == "-"{
+                negative = true
+                    
+            } else {
+                var nextNum = Int(nums[index])
+                if negative {
+                    nextNum = nextNum! * 1
+                    negative = false
+                        
+                }
+                    
+                average += nextNum!
+            }
+                
+            return average / count
+                
+        }
+    } else {
+        return 0
     }
 }
+    
+    
+    
+public func calculate(_ arg: String) -> Int {
+    return calculate( arg.split(separator: " ").map({ substr in String(substr) }) )
+}
 
-print("UW Calculator v1")
-print("Enter an expression separated by returns:")
-let first = readLine()!
-let operation = readLine()!
-let second = readLine()!
-print(Calculator().calculate([first, operation, second]))
+//print("UW Calculator v1")
+//print("Enter an expression separated by returns:")
+//let first = readLine()!
+//let operation = readLine()!
+//let second = readLine()!
+//print(Calculator().calculate([first, operation, second]))
+//
 
